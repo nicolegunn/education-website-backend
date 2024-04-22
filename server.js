cb34d445bbbfbe8f85e0368c0f7a8a9fc948529a
maata <maatat@missionreadyhq.com>
2024-04-23 11:51:07 +1200

    pushing backend

--- server.js
+++ server.js
@@ -160,20 +160,23 @@ app.get("/student", (req, res) => {
   });
 });
 
-app.get("/students-with-projects", (req, res) => {
-  const query = 'SELECT s.student_id, s.name, COUNT(p.date_completed) AS completed_projects_count ' +
-  'FROM student s ' +
-  'LEFT JOIN student_projects p ON s.student_id = p.student_id ' +
-  'GROUP BY s.student_id, s.name;'
+//progress-tracker
+app.get("/student-progress-tracker", (req,res) => {
+  const query = "SELECT s.student_id, s.name, COUNT(p.date_completed) AS completed_projects_count, JSON_ARRAYAGG(JSON_OBJECT('project_id', p.project_id, 'project_details', p.date_completed)) AS projects " +
+'FROM student s ' +
+'JOIN student_projects p ON s.student_id = p.student_id ' +
+'GROUP BY s.student_id ' +
+'ORDER BY s.student_id;'
   pool.execute(query, (error, results) => {
-    if (error) {
+    if(error) {
       console.error(error);
-      return res.status(500).json({ error: "Internal Server Error" });
+      return res.status(500).json( { error: "internal Server Error" });
     }
     return res.json(results);
   })
 })
 
+app.get
 app
   .listen(PORT, () => {
     console.log(`Server is alive on http://localhost:${PORT}`);
